27d1dd8aac042651e4069f5904ab632a992568d1
Bill Burke <burke187@gmail.com>
2014-10-12 14:52:00 -0500

    added dashboard

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,4 @@
 angular.module('Wegotyoscripts')
     .controller('HomeCtrl', function ($scope) {
-        $scope.things = ['login', 'make it work'];
+        $scope.things = ['login', 'do work son'];
     });
