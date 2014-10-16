fd02b6037924541c513a5579544ad5bc5c54938f
Bill Burke <burke187@gmail.com>
2014-10-16 10:45:31 -0500

    add/delete buttons added

--- app/assets/javascripts/ng-app/controllers/scriptCtrl.js
+++ app/assets/javascripts/ng-app/controllers/scriptCtrl.js
@@ -2,4 +2,14 @@ angular.module('Wegotyoscripts')
     .controller('scriptCtrl', function ($scope){
     $scope.title1 = "Select Prescription Type";
     $scope.items = ["Glasses", "Contacts", "Both"];
+    $scope.allScripts = [];
+    
+    $scope.addScript = function() {
+        $scope.allScripts.push({});
+    };
+    $scope.removeScript = function(script) {
+        var i = _($scope.allScripts).indexOf(script);
+        if (i >= 0) $scope.allScripts.splice(i, 1);
+    };    
+}
 });
